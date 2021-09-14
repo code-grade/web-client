589e1a180ae2478177497147a17cd15e1f608862
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-09-14 21:54:16 +0530

    FIX auth page alignment

--- src/views/sign-in/index.vue
+++ src/views/sign-in/index.vue
@@ -8,7 +8,7 @@
               <v-window v-model="step">
                 <v-window-item :value="1">
                   <v-row class="fill-height pa-3">
-                    <v-col cols="12" md="6">
+                    <v-col class="login__col" cols="12" md="6">
                       <v-card-text class="mt-12">
                         <h1
                           class="text-center display-2 secondary--text text--accent-3"
@@ -20,11 +20,13 @@
                             color="secondary darken-2"
                             label="Email"
                             prepend-icon="mdi-email"
+                          
                           ></v-text-field>
                           <v-text-field
                            color="secondary darken-2"
                             label="Password"
                             prepend-icon="mdi-lock"
+                          
                           ></v-text-field>
                          
 
@@ -32,11 +34,11 @@
                         </v-form>
                         <h3 class="text-center mt-4">Forgot your password ?</h3>
                       </v-card-text>
-                      <div class="text-center ma-3">
+                      <div class="text-center ">
                         <v-btn rounded color="secondary accent-3" dark>SIGN IN</v-btn>
                       </div>
                     </v-col>
-                    <v-col cols="12" md="6" >
+                    <v-col class="login__col" cols="12" md="6" >
                       <v-card-text class="secondary--text mt-12">
                         <h1 class="text-center display-1">New User?</h1>
                         <h5
@@ -58,7 +60,7 @@
                 </v-window-item>
                 <v-window-item :value="2">
                   <v-row class="fill-height pa-3">
-                    <v-col cols="12" md="6" class="pa-2">
+                    <v-col class="login__col" cols="12" md="6" >
                       <v-card-text class="secondary--text mt-12">
                         <h1 class="text-center display-1">Already memeber?</h1>
                         <h5
@@ -76,37 +78,90 @@
                       </div>
                     </v-col>
 
-                    <v-col cols="12" md="6">
+                    <v-col class="login__col" cols="12" md="6">
                       <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 secondary--text text--accent-3">Create Account</h1>
+                        <h1 class="text-center display-2 secondary--text text--accent-3 mb-3">Create Account</h1>
                         
                         
                         <v-form>
                           
-                         
+                        
                          <v-text-field
                             color="secondary darken-2"
-                            label="Full Name"
+                            label="User Name"
                             prepend-icon="mdi-account"
+                            dense
                           ></v-text-field>
+                          <v-row>
+                          <v-col
+                            cols="12"
+                            md="6"
+                          >
+                          <v-text-field
+                            color="secondary darken-2"
+                            label="First Name"
+                            prepend-icon="mdi-account"
+                            dense
+                          ></v-text-field>
+                          
+                          </v-col>
+                          <v-col
+                            cols="12"
+                            md="6"
+                          >
+                          <v-text-field
+                            color="secondary darken-2"
+                            label="Last Name"
+                            prepend-icon="mdi-account"
+                            dense
+                          ></v-text-field>
+                          
+                          </v-col>
+                        </v-row>
                          <v-text-field
                             color="secondary darken-2"
                             label="Email"
                             prepend-icon="mdi-email"
+                            dense
                           ></v-text-field>
                           <v-text-field
                            color="secondary darken-2"
                             label="Password"
                             prepend-icon="mdi-lock"
+                            dense
                           ></v-text-field>
                           <v-text-field
                            color="secondary darken-2"
                             label="Confirm Password"
                             prepend-icon="mdi-lock"
+                            dense
                           ></v-text-field>
+                         
+                          <p>User Role</p>
+
+                          <v-btn-toggle
+                            v-model="icon"
+                            borderless
+                            dense
+                            class="pa-0 ma-0"
+                          >
+                            <v-btn value="left">
+                              <span class="hidden-sm-and-down">Student</span>
+                              <v-icon right>
+                                mdi-format-align-left
+                              </v-icon>
+                            </v-btn>
+
+                            <v-btn value="center">
+                              <span class="hidden-sm-and-down">Instructor</span>
+                              <v-icon right>
+                                mdi-format-align-center
+                              </v-icon>
+                            </v-btn>                               
+                          </v-btn-toggle>                             
                         </v-form>
                       </v-card-text>
-                      <div class="text-center mt-n5">
+                      <div class="text-center mt-5">
                         <v-btn rounded color="secondary accent-3" dark>SIGN UP</v-btn>
                       </div>
                     </v-col>
@@ -131,3 +186,16 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+  .login__col{
+     display: flex;
+  flex-direction: column;
+
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  
+  
+    
+  }
+</style>
