14f85b0d8326e3f89bda31688a968b19addcf8ca
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-09-25 12:00:08 +0530

    ADD API + user profile view

--- src/views/app/user-profile/index.vue
+++ src/views/app/user-profile/index.vue
@@ -14,168 +14,248 @@
           cols="12"
           md="8"
       >
-        <v-card class="pa-5">
-          <v-form>
-            <v-container class="py-0">
-              <v-row>
-                <v-col
-                    cols="4"
+
+            <v-card class="pa-5">
+              <v-form>
+                <v-container class="py-0">
+                  <v-row>
+                    <v-col
+                        cols="4"
 
 
-                >
-                  <div class="font-weight-medium pt-5">
-                    User Name
+                    >
+                      <div class="font-weight-medium pt-5">
+                        User Name
 
-                  </div>
+                      </div>
 
-                </v-col>
-                <v-col
-                    cols="8"
-
-
-                >
-                  <v-text-field
-                      label="User name (disabled)"
-                      disabled
-                  />
-                </v-col>
-                <v-col
-                    cols="4"
+                    </v-col>
+                    <v-col
+                        cols="8"
 
 
-                >
-                  <div class="font-weight-medium pt-5">
-                    First Name
-
-                  </div>
-
-                </v-col>
-
-                <v-col
-                    cols="8"
-
-                >
-                  <v-text-field
-                      label="First Name"
-                      color="secondary darken-2"
-                  />
-                </v-col>
-                <v-col
-                    cols="4"
+                    >
+                      <v-text-field
+                          label="User name (disabled)"
+                          disabled
+                          v-model="userName"
+                      />
+                    </v-col>
+                    <v-col
+                        cols="4"
 
 
-                >
-                  <div class="font-weight-medium pt-5">
-                    User Name
+                    >
+                      <div class="font-weight-medium pt-5">
+                        First Name
 
-                  </div>
+                      </div>
 
-                </v-col>
+                    </v-col>
 
-                <v-col
-                    cols="8"
+                    <v-col
+                        cols="8"
 
-                >
-                  <v-text-field
-                      label="Last Name"
-                      color="secondary darken-2"
-                  />
-                </v-col>
-                <v-col
-                    cols="4"
-
-
-                >
-                  <div class="font-weight-medium pt-5">
-                    Email
-
-                  </div>
-
-                </v-col>
-
-                <v-col cols="8"
-                       >
-                  <v-text-field
-                      label="Email"
-                      color="secondary darken-2"
-                  />
-                </v-col>
-                <v-col
-                    cols="4"
+                    >
+                      <v-text-field
+                          label="First Name"
+                          v-model="firstName"
+                          color="secondary darken-2"
+                      />
+                    </v-col>
+                    <v-col
+                        cols="4"
 
 
-                >
-                  <div class="font-weight-medium pt-5">
+                    >
+                      <div class="font-weight-medium pt-5">
+                        User Name
+
+                      </div>
+
+                    </v-col>
+
+                    <v-col
+                        cols="8"
+
+                    >
+                      <v-text-field
+                          label="Last Name"
+                          v-model="lastName"
+                          color="secondary darken-2"
+                      />
+                    </v-col>
+                    <v-col
+                        cols="4"
 
 
-                  </div>
+                    >
+                      <div class="font-weight-medium pt-5">
+                        Email
 
-                </v-col>
+                      </div>
 
-                <v-col
-                    cols="8"
-                    class="text-left"
-                >
-                  <v-btn
-                      color="primary"
-                      class="mt-0"
-                  >
-                    Save Changes
-                  </v-btn>
-                </v-col>
-                <v-col
-                    cols="4"
+                    </v-col>
 
-
-                >
-                  <div class="font-weight-medium pt-5">
+                    <v-col cols="8"
+                    >
+                      <v-text-field
+                          label="Email"
+                          color="secondary darken-2"
+                          v-model="email"
+                      />
+                    </v-col>
+                    <v-col
+                        cols="4"
 
 
-                  </div>
-
-                </v-col>
-                <v-col
-                    cols="8"
-                    class="text-left"
-                >
-                  <v-btn
-                      color="secondary"
-                      class="mr-0 mb-0"
+                    >
+                      <div class="font-weight-medium pt-5">
 
 
-                  >
-                    Reset Password
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card>
+                      </div>
+
+                    </v-col>
+
+                    <v-col
+                        cols="8"
+                        class="text-left"
+                    >
+                      <v-btn
+                          color="primary"
+                          class="mt-0"
+                      >
+                        Save Changes
+                      </v-btn>
+                    </v-col>
+                    <v-col
+                        cols="4"
+
+
+                    >
+                      <div class="font-weight-medium pt-5">
+
+
+                      </div>
+
+                    </v-col>
+                    <v-col
+                        cols="8"
+                        class="text-left"
+                    >
+                      <v-btn
+                          color="secondary"
+                          class="mr-0 mb-0"
+
+
+                      >
+                        Reset Password
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card>
+
+
       </v-col>
 
       <v-col
           cols="12"
           md="4"
       >
-        <v-card
-            class="v-card-profile"
-            avatar="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg"
-        >
-          <v-card-text class="text-center">
-            <v-list-item>
-              <v-list-item-avatar size="100">
-                <img
-                    src="https://www.w3schools.com/howto/img_avatar.png"
-                    alt="John"
+        <v-row>
+          <v-col cols="12">
+            <v-card
+                class="v-card-profile"
+                avatar="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg"
+            >
+              <v-card-text class="text-center">
+                <v-row>
+                  <v-col cols="12" class="text-center mb-0">
+                    <v-avatar
+                        color="primary"
+                        size="112"
+
+                    >
+                      <img
+                          src="https://www.w3schools.com/howto/img_avatar.png"
+                          alt="John"
+                      >
+
+                    </v-avatar>
+
+                  </v-col>
+                  <v-col cols="12" class="mt-0">
+
+                    <v-list-item-title class="title text-center" >{{userName}}</v-list-item-title>
+                    <v-list-item-subtitle class="text-center">{{user_role}}</v-list-item-subtitle>
+
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card>
+              <div class="text-center">
+
+
+
+
+                <v-chip
+                    class="ma-2 pa-4"
+                    color="green"
+                    text-color="white"
+                    v-if="isEnabled"
+                    v-else
                 >
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="title text-left" style="margin-top:20px;">Musab Mahmoodh</v-list-item-title>
-                <v-list-item-subtitle class="text-left">musab@</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-card-text>
-        </v-card>
+                  <v-icon left>
+                    mdi-archive-lock-open-outline
+                  </v-icon>
+                  Enabled
+                </v-chip>
+                <v-chip
+                    class="ma-2 pa-4"
+                    color="red"
+                    text-color="white"
+                    v-else
+                >
+                  <v-icon left>
+                    mdi-archive-lock-outline
+                  </v-icon>
+                  Not Enabled
+                </v-chip>
+
+                <v-chip
+                    class="ma-2 pa-4"
+                    color="red"
+                    text-color="white"
+                    v-if="verified"
+                >
+                  <v-icon left>
+                    mdi-account-multiple-check-outline
+                  </v-icon>
+
+                  Verified
+                </v-chip>
+
+                <v-chip
+                    class="ma-2 pa-4"
+                    color="red"
+                    text-color="white"
+                    v-else
+                >
+                  <v-icon left>
+                    mdi-account-multiple-remove-outline
+                  </v-icon>
+                  Un Verified
+                </v-chip>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+
       </v-col>
     </v-row>
   </v-container>
@@ -191,8 +271,11 @@ export default {
     firstName:"",
     lastName:"",
     email:"",
-    user_role:"STUDENT",
+    user_role:"",
+    isEnabled:false,
+    verified:false,
     valid: true,
+
     nameRules :[Validators.required(),Validators.min(5),Validators.max(15)],
     emailRules:[Validators.email()],
     loading: false,
@@ -209,8 +292,14 @@ export default {
 
       this.fetchingDataLoader = false;
       if (status.status === 200) {
-
-        this.users = [...res_data]
+        const {username,email:{email,verified},firstName,isEnabled,lastName,role} = res_data
+        this.userName=username;
+        this.email=email
+        this.firstName=firstName
+        this.lastName=lastName
+        this.user_role = role
+        this.isEnabled= isEnabled
+        this.verified = verified
       } else {
         this.$vToastify.error(res_data, "Done")
       }
