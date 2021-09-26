d626766b7a51da1bb729ce9d82bb24d2f0dd8d3f
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-09-26 17:18:12 +0530

    add new module to api

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import user from "./modules/user"
 import question from "./modules/question"
+import assignment from "./modules/assignment";
 
 export const api = {
     user, 
     question,
+    assignment
 }
 
 export default api;
