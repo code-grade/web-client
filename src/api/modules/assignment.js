2a953a4d1eeae9119f86efddf53c6d2fa1f5286b
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-09-26 17:17:26 +0530

    add assignment module

--- src/api/modules/assignment.js
+++ src/api/modules/assignment.js
@@ -3,24 +3,42 @@ import {axios, extractDataResolve} from "../client";
 export const assignment = {
     /**
      * Create new assignment
-     * @param data
+     * @param data {*} assignment data
      * @returns {Promise<[*, *]>}
      */
-    create: (data) => extractDataResolve(axios.post("/api/question", data)),
+    create: (data) => extractDataResolve(axios.post("/api/assignment", data)),
 
     /**
      * Get all assignment of current user (instructor)
      * @param state - DRAFT | PUBLISHED | OPEN | CLOSED | SCHEDULED | FINALIZED
      * @returns {Promise<[*, *]>}
      */
-    all: (state) => extractDataResolve(
-        axios.get("/api/auth/question/all", {
+    instructor: (state) => extractDataResolve(
+        axios.get("/api/assignment", {
             params: {state}
         })
     ),
 
-    update:(questionId,data) => extractDataResolve(axios.put(`/api/auth/question/${questionId}`, data)),
-    delete:(questionId)=>extractDataResolve(axios.delete(`/api/auth/question/${questionId}`)),
+    participate: {
+        /**
+         * Enroll to an assignment
+         *  Participate to an assignment as current user (Student)
+         * @param assignmentId {string} UUID
+         * @returns {Promise<[*, *]>}
+         */
+        student: (assignmentId) => extractDataResolve(
+            axios.put(`/api/participate/${assignmentId}`)
+        ),
+
+        /**
+         * Get all enrolled assignment for current user (Student)
+         * @param state - DRAFT | PUBLISHED | OPEN | CLOSED | SCHEDULED | FINALIZED
+         * @returns {Promise<[*, *]>}
+         */
+        all: (state) => extractDataResolve(
+           axios.get("/assignment/participate", {params: {state}})
+        )
+    }
 }
 
-export default question;
+export default assignment;
