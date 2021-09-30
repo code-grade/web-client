9e2638ab132b8f8d3e1ee09c98ee34e5d058f5b8
NethminiJPU <61895366+NethminiJPU@users.noreply.github.com>
2021-09-30 22:57:38 +0530

    fixed an error in api

--- src/api/modules/assignment.js
+++ src/api/modules/assignment.js
@@ -78,8 +78,10 @@ export const assignment = {
         axios.put(`/api/assignment/${assignmentId}/state/${state}`)
     ),
 
-    getPublished:() => extractDataResolve(
-        axios.get(`/api/assignment/public`,{params:state})
+    getPublished:(state) => extractDataResolve(
+        axios.get(`/api/assignment/public`,{
+            params:{state}
+        })
     ),
 
     grade:(assignmentId,userId,data) => extractDataResolve(
