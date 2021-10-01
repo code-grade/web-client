fb721f3c5438e04977271779b7c6cd806b4ccc10
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-10-01 16:51:37 +0530

    create assignment update

--- src/views/app/create-assignment/index.vue
+++ src/views/app/create-assignment/index.vue
@@ -11,16 +11,16 @@
     </v-row>
 
     <v-stepper
-        v-model="e1"
+        v-model="step"
         class="ma-5"
         vertical
     >
 
       <v-stepper-step
-          :complete="e1 > 1"
+          :complete="step > 1"
           style="cursor: pointer"
           complete-icon="mdi-check-bold"
-          @click="e1 = 1"
+          @click="step = 1"
           step="1"
       >
         Basic Details
@@ -32,42 +32,22 @@
             ref="assignmentForm"
             id="qn-info-form"
             v-model="valid"
-            @submit.prevent="e1 = 2"
+            @submit.prevent="step = 2"
             lazy-validation
         >
           <v-row>
-            <v-col
-                cols="12"
-                sm="6"
-                md="8"
-            >
-              <v-row>
-                <v-col>
-                  <h5>Title</h5>
-                  <v-text-field
-                      dense
-                      outlined
-                      v-model="title"
-                      :rules="titleRules"
-                      placeholder="Title of the assignment"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <h5>Description</h5>
-                  <v-flex class="mb-2">
-                    <vue-editor v-model="description" :rules="descriptionRules"/>
-                  </v-flex>
-                </v-col>
-              </v-row>
+            <v-col cols="12" sm="6" md="8">
+              <h5 class="mb-2">Title</h5>
+              <v-text-field
+                  dense
+                  outlined
+                  v-model="title"
+                  :rules="titleRules"
+                  placeholder="Title of the assignment"
+              ></v-text-field>
             </v-col>
-            <v-col
-                cols="12"
-                sm="6"
-                md="3"
-            >
-              <h5>Set question privacy</h5>
+            <v-col cols="12" sm="6" md="4">
+              <h5 class="mb-2">Set question privacy</h5>
               <v-select
                   dense
                   :items="types"
@@ -76,22 +56,41 @@
                   outlined
                   placeholder="Select Type"
               ></v-select>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <h5 class="mb-2">Description</h5>
+              <v-flex>
+                <vue-editor v-model="description" :rules="descriptionRules"/>
+              </v-flex>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
               <v-checkbox
                   v-model="scheduled"
                   label="Scheduled Assignment"
+                  hide-details
               ></v-checkbox>
-              <div v-if="scheduled">
-                <v-datetime-picker
-                    v-model="openTime"
-                    label="Select opening time"
-                    class="secondary"
-                />
-                <v-datetime-picker
-                    v-model="closeTime"
-                    label="Select closing time"
-                    class="secondary"
-                />
-              </div>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-datetime-picker
+                  :disabled="!scheduled"
+                  v-model="openTime"
+                  label="Select opening time"
+                  class="secondary"
+              />
+            </v-col>
+            <v-col>
+              <v-datetime-picker
+                  :disabled="!scheduled"
+                  v-model="closeTime"
+                  label="Select closing time"
+                  class="secondary"
+              />
             </v-col>
           </v-row>
           <v-divider class="mt-5 mb-5"/>
@@ -114,10 +113,10 @@
 
       <v-stepper-step
           step="2"
-          :complete="e1 > 2"
+          :complete="step > 2"
           style="cursor: pointer"
           complete-icon="mdi-check-bold"
-          @click="e1 = 2"
+          @click="step = 2"
       >
         Add Questions
       </v-stepper-step>
@@ -143,7 +142,7 @@
                 outlined
                 min-width="100"
                 class="ml-2 mr-2"
-                @click="e1 = 1"
+                @click="step = 1"
             >
               Back
             </v-btn>
@@ -151,6 +150,7 @@
                 color="primary"
                 min-width="100"
                 class="ml-2 mr-2"
+                :loading="loading"
                 @click="submitAssignment"
                 dark
             >
@@ -177,7 +177,8 @@ export default {
   components: {QuestionCollection, AssignmentInfo, VueEditor},
   data: () => ({
     valid: true,
-    e1: 1,
+    loading: false,
+    step: 1,
     title: '',
     description: '',
     type: 'PUBLIC',
@@ -192,8 +193,6 @@ export default {
     selectLanguage: null,
     types: ['PUBLIC', 'PRIVATE'],
     checkbox: false,
-    dialog: false,
-    dialogDelete: false,
     selected: [],
     headers: [
       {
@@ -223,6 +222,15 @@ export default {
       }
     },
     async submitAssignment() {
+      await this.$refs.assignmentForm.validate()
+      if (!this.valid || this.description === "") {
+        await this.$pop.info({
+          title: 'Info',
+          text: 'Incomplete form data please fill all necessary fields.',
+        })
+        return;
+      }
+
       const {button} = await this.$pop.info({
         title: 'Confirm',
         text: 'Are you want to submit?',
@@ -231,26 +239,25 @@ export default {
       if (button !== 1) return
 
       const selectedQuestions = this.selected.map(q => q.questionId)
-      if (this.title !== '' && this.description !== "") {
-        const assignmentData = {
-          title: this.title,
-          description: this.description,
-          type: this.type,
-          openTime: this.openTime,
-          closeTime: this.closeTime,
-          questions: selectedQuestions,
-          scheduled: this.scheduled
-        }
-        const [status, res_data] = await api.assignment.create(assignmentData)
-        if (status.status === 200) {
-          this.$vToastify.success(status.message, "Successfully Added!")
-          this.$refs.assignmentForm.reset()
-          await router.push("/app/assignments")
-        } else {
-          this.$vToastify.error(res_data, "Done")
-        }
+      const assignmentData = {
+        title: this.title,
+        description: this.description,
+        type: this.type,
+        openTime: this.openTime,
+        closeTime: this.closeTime,
+        questions: selectedQuestions,
+        scheduled: this.scheduled
+      }
+      console.log(assignmentData)
+      this.loading = true
+      const [status, res_data] = await api.assignment.create(assignmentData)
+      this.loading = false
+      if (status.status === 200) {
+        this.$vToastify.success(status.message, "Successfully Added!")
+        this.$refs.assignmentForm.reset()
+        await router.push("/app/assignments")
       } else {
-        this.$vToastify.error("Please fill all the fields", "Incomplete Data")
+        this.$vToastify.error(res_data, "Done")
       }
     }
   },
