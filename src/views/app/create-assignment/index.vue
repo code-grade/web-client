11bcc4a3ff635a093178dd2e7f35b1239af4f2b5
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-10-01 14:07:14 +0530

    create assignment update

--- src/views/app/create-assignment/index.vue
+++ src/views/app/create-assignment/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <v-row
-        class="ma-10"
-    >
+  <v-container class="">
+    <v-row class="ml-10 mr-10 mb-5 mt-5">
       <v-flex
           style="float:left">
         <h2>Create New Assignment
@@ -11,170 +9,158 @@
       </v-flex>
 
     </v-row>
+
     <v-stepper
         v-model="e1"
         class="ma-5"
+        vertical
     >
-      <v-stepper-header>
-        <v-stepper-step
-            :complete="e1 > 1"
-            step="1"
+
+      <v-stepper-step
+          :complete="e1 > 1"
+          style="cursor: pointer"
+          complete-icon="mdi-check-bold"
+          @click="e1 = 1"
+          step="1"
+      >
+        Basic Details
+      </v-stepper-step>
+
+      <v-stepper-content step="1">
+        <v-form
+            class="mb-5 pa-5"
+            ref="assignmentForm"
+            id="qn-info-form"
+            v-model="valid"
+            @submit.prevent="e1 = 2"
+            lazy-validation
         >
-          Assignment Details
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step
-            :complete="e1 > 2"
-            step="2"
-        >
-          Add Questions
-        </v-stepper-step>
-        <v-divider></v-divider>
-        <v-stepper-step step="3">
-          Confirm
-        </v-stepper-step>
-      </v-stepper-header>
-
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <!--            <AssignmentInfo/>-->
-          <v-form class="rounded-lg elevation-3 mb-5 pa-5"
-                  ref="assignmentForm"
-                  id="qn-info-form"
-                  v-model="valid"
-                  @submit.prevent="sendToAddQuestions"
-                  lazy-validation>
-            <v-row>
-              <v-col
-                  cols="12"
-                  sm="6"
-                  md="8"
-              >
-                <v-row>
-                  <v-col>
-                    <h5>Title</h5>
-                    <v-text-field
-                        dense
-                        outlined
-                        v-model="title"
-                        :rules="titleRules"
-                        placeholder="Title of the assignment"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col>
-                    <h5>Description</h5>
-                    <v-flex class="mb-2">
-                      <vue-editor v-model="description" :rules="descriptionRules"/>
-                    </v-flex>
-                  </v-col>
-                </v-row>
-              </v-col>
-              <v-col
-                  cols="12"
-                  sm="6"
-                  md="3"
-              >
-                <h5>Set question privacy</h5>
-                <v-select
-                    dense
-                    :items="types"
-                    v-model="type"
-                    :rules="difficultyRules"
-                    outlined
-                    placeholder="Select Type"
-                ></v-select>
-                <v-checkbox
-                    v-model="scheduled"
-                    :label="`Scheduled Assignment`"
-                ></v-checkbox>
-                <div v-if="scheduled">
-                  <v-datetime-picker label="Select start Datetime" class="secondary"
-                                     v-model="openTime"></v-datetime-picker>
-                  <v-datetime-picker label="Select end Datetime" class="secondary"
-                                     v-model="closeTime"></v-datetime-picker>
-                </div>
-              </v-col>
-            </v-row>
-
-
-          </v-form>
-          <div class="text-right ">
-            <v-btn
-                :disabled="!valid" type="submit" form="qn-info-form" class="primary "
+          <v-row>
+            <v-col
+                cols="12"
+                sm="6"
+                md="8"
             >
-              Next
-            </v-btn>
-          </div>
-        </v-stepper-content>
+              <v-row>
+                <v-col>
+                  <h5>Title</h5>
+                  <v-text-field
+                      dense
+                      outlined
+                      v-model="title"
+                      :rules="titleRules"
+                      placeholder="Title of the assignment"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <h5>Description</h5>
+                  <v-flex class="mb-2">
+                    <vue-editor v-model="description" :rules="descriptionRules"/>
+                  </v-flex>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col
+                cols="12"
+                sm="6"
+                md="3"
+            >
+              <h5>Set question privacy</h5>
+              <v-select
+                  dense
+                  :items="types"
+                  v-model="type"
+                  :rules="difficultyRules"
+                  outlined
+                  placeholder="Select Type"
+              ></v-select>
+              <v-checkbox
+                  v-model="scheduled"
+                  label="Scheduled Assignment"
+              ></v-checkbox>
+              <div v-if="scheduled">
+                <v-datetime-picker
+                    v-model="openTime"
+                    label="Select opening time"
+                    class="secondary"
+                />
+                <v-datetime-picker
+                    v-model="closeTime"
+                    label="Select closing time"
+                    class="secondary"
+                />
+              </div>
+            </v-col>
+          </v-row>
+          <v-divider class="mt-5 mb-5"/>
+          <v-row>
+            <v-col cols="12" class="text-right">
+              <v-btn
+                  :disabled="!valid"
+                  type="submit"
+                  form="qn-info-form"
+                  color="primary"
+                  min-width="100"
+                  class="ml-2 mr-2"
+              >
+                Next
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-stepper-content>
 
-        <v-stepper-content step="2">
-          <!--            <QuestionCollection/>-->
-          <v-data-table
-              v-model="selected"
-              :headers="headers"
-              :items="questions"
-              :single-select="false"
-              item-key="questionId"
-              show-select
-              class="elevation-1 mb-5"
+      <v-stepper-step
+          step="2"
+          :complete="e1 > 2"
+          style="cursor: pointer"
+          complete-icon="mdi-check-bold"
+          @click="e1 = 2"
+      >
+        Add Questions
+      </v-stepper-step>
 
-          ></v-data-table>
-          <div class="text-right ">
-            <v-btn text @click="e1 = 1">
+      <v-stepper-content step="2">
+        <v-row>
+          <v-col>
+            <v-data-table
+                v-model="selected"
+                :headers="headers"
+                :items="questions"
+                :single-select="false"
+                item-key="questionId"
+                show-select
+                class="mb-5"
+            />
+          </v-col>
+        </v-row>
+        <v-divider class="mt-5 mb-5"/>
+        <v-row>
+          <v-col class="text-right">
+            <v-btn
+                outlined
+                min-width="100"
+                class="ml-2 mr-2"
+                @click="e1 = 1"
+            >
               Back
             </v-btn>
             <v-btn
                 color="primary"
-                @click="e1 = 3"
+                min-width="100"
+                class="ml-2 mr-2"
+                @click="submitAssignment"
                 dark
             >
-              Next
+              Submit
             </v-btn>
-
-          </div>
-
-
-        </v-stepper-content>
-
-        <v-stepper-content step="3">
-          <v-card
-              class="mb-12"
-
-              height="200px"
-
-          >
-            <v-card-title primary-title class="justify-center">
-              <div>
-                <h1 class="text--center text-center ">Confirm</h1>
-                <h4>Are you sure, you want to create this assignment?</h4>
-              </div>
-            </v-card-title>
-            <v-card-actions class="justify-center">
-              <v-btn
-                  color="success"
-                  @click="createAssignment"
-              >
-                Submit
-              </v-btn>
-            </v-card-actions>
-
-          </v-card>
-          <div class="text-right ">
-            <v-btn text @click="e1 = 2">
-              Back
-            </v-btn>
-
-          </div>
-
-
-        </v-stepper-content>
-      </v-stepper-items>
+          </v-col>
+        </v-row>
+      </v-stepper-content>
     </v-stepper>
-  </div>
+  </v-container>
 </template>
 
 
@@ -189,9 +175,7 @@ import router from "@/router"
 export default {
   name: "index",
   components: {QuestionCollection, AssignmentInfo, VueEditor},
-
   data: () => ({
-
     valid: true,
     e1: 1,
     title: '',
@@ -199,7 +183,7 @@ export default {
     type: 'PUBLIC',
     openTime: '',
     closeTime: '',
-    scheduled: false,
+    scheduled: true,
     titleRules: [Validators.required()],
     nameRules: [Validators.required()],
     selectRules: [Validators.required()],
@@ -223,11 +207,9 @@ export default {
     ],
     questions: [],
   }),
-
   created() {
     this.initialize()
   },
-
 
   methods: {
     async initialize() {
@@ -235,41 +217,35 @@ export default {
       const [status, res_data] = await api.question.instructor()
       this.loading = false;
       if (status.status === 200) {
-
         this.questions = [...res_data]
       } else {
         this.$vToastify.error(res_data, "Done")
       }
     },
-    sendToAddQuestions() {
-      this.e1 = '2'
-    },
-    async createAssignment() {
-      const selectedQuestions = []
-      this.selected.forEach(q => {
-        selectedQuestions.push(q.questionId)
+    async submitAssignment() {
+      const {button} = await this.$pop.info({
+        title: 'Confirm',
+        text: 'Are you want to submit?',
+        ok: 'YES', cancel: 'NO'
       })
+      if (button !== 1) return
+
+      const selectedQuestions = this.selected.map(q => q.questionId)
       if (this.title !== '' && this.description !== "") {
-
-
-        let assignmentData =
-            {
-              title: this.title,
-              description: this.description,
-              type: this.type,
-              openTime: this.openTime,
-              closeTime: this.closeTime,
-              questions: selectedQuestions,
-              scheduled: this.scheduled
-            }
+        const assignmentData = {
+          title: this.title,
+          description: this.description,
+          type: this.type,
+          openTime: this.openTime,
+          closeTime: this.closeTime,
+          questions: selectedQuestions,
+          scheduled: this.scheduled
+        }
         const [status, res_data] = await api.assignment.create(assignmentData)
-
-
         if (status.status === 200) {
           this.$vToastify.success(status.message, "Successfully Added!")
           this.$refs.assignmentForm.reset()
           await router.push("/app/assignments")
-
         } else {
           this.$vToastify.error(res_data, "Done")
         }
